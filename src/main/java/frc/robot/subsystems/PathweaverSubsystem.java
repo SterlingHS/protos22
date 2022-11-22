1b5a42ed1ef8c4fcd7fb10d9b2bf98e0771ae3e9
Isaiah Phillips <jasonmcgee4039@gmail.com>
2022-11-22 12:09:49 -0600

    Added PathweaverSubsystem; added function call to robotInit

---
+++ src/main/java/frc/robot/subsystems/PathweaverSubsystem.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+
+public class PathweaverSubsystem {
+    String trajectoryJSON = "paths/YourPath.wpilib.json"; //Put actual file path in place of YourPath
+    Trajectory trajectory = new Trajectory();
+
+
+    public void PathWeaverInit() {
+        try {
+            Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+            trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+        } catch (IOException ex) {
+            DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+        }
+    }
+}
