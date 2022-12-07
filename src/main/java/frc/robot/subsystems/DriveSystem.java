dde3abf18cb579d6be53fb149603ef82251359ff
Elias Jensen <Goldenchesstrex@gmail.com>
2022-12-07 10:30:53 -0600

    we added odometry

--- src/main/java/frc/robot/subsystems/DriveSystem.java
+++ src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -5,9 +5,9 @@ import frc.robot.Constants;
 import frc.robot.RobotMap;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-//import com.kauailabs.navx.frc.AHRS;
+import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.drive.*;
-//import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 //import edu.wpi.first.math.geometry.Pose2d;
 //import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
@@ -27,13 +27,16 @@ public class DriveSystem extends SubsystemBase {
     private MotorControllerGroup mLeft = new MotorControllerGroup(leftFront, leftRear);
     private MotorControllerGroup mRight = new MotorControllerGroup(rightFront, rightRear);
     private DifferentialDrive mDrive = new DifferentialDrive(mLeft, mRight);
+    DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(
+    getGyroHeading(), new Pose2d(5.0, 13.5, new Rotation2d()));
 
     //public static final DifferentailDriveKinematics KDriveKinematics = new DifferentailDriveKinematics(kTrackWidthMeters);
 
 
     private Encoder rightEncoder = new Encoder(Constants.ENCODER_RIGHT_A, Constants.ENCODER_RIGHT_B);
     private Encoder leftEncoder = new Encoder(Constants.ENCODER_LEFT_A, Constants.ENCODER_LEFT_B);
-    //private AHRS navx_device = new AHRS(SerialPort.Port.kMXP);
+    protected static Field2d trajField = new Field2d(); //FOR PATHWEAVER
+    private AHRS navx_device = new AHRS(SerialPort.Port.kMXP);
 
     public DriveSystem() 
     {
@@ -45,7 +48,8 @@ public class DriveSystem extends SubsystemBase {
         rightEncoder.setDistancePerPulse(10./2208.);
         leftEncoder.setDistancePerPulse(10./2208.);
         //2208 pulses per 10ft
-        // navx_device.enableLogging(true);
+         navx_device.enableLogging(true);
+        SmartDashboard.putData("trajField", trajField);
     }
 
     @Override
@@ -127,7 +131,7 @@ public class DriveSystem extends SubsystemBase {
         
     }
 
-    /* 
+    
     public void calibrateGyro()
     {
         navx_device.calibrate();
@@ -158,10 +162,104 @@ public class DriveSystem extends SubsystemBase {
         }
         return correctedAngle;
     }
-    */
     
-}
+    public double getPitch() {
+        return navx_device.getPitch();
+    }
 
+    public double getRoll() {
+        return navx_device.getRoll();
+    }
+
+    public double getCompassHeading() {
+        return navx_device.getCompassHeading();
+    }
+
+    public double getFusedHeading() {
+        return navx_device.getFusedHeading();
+    }
+
+    public double getLinearWorldAccelX() {
+        return navx_device.getWorldLinearAccelX();
+    }
+
+    public double getLinearWorldAccelY() {
+        return navx_device.getWorldLinearAccelY();
+    }
+
+    public double getLinearWorldAccelZ() {
+        return navx_device.getWorldLinearAccelZ();
+    }
+
+    public static void runTraj(Trajectory trajectory, double timeInSec) {
+        Trajectory.State goal = trajectory.sample(timeInSec);
+        trajField.setRobotPose(goal.poseMeters);
+
+        var currentPose = odometry.getPoseMeters();
+
+        var chaspeed = ramseteController.calculate(currentPose, goal);
+
+        var wheelSpeeds = kinematic.toWheelSpeeds(chaspeed); // left right speed
+        double left = wheelSpeeds.leftMetersPerSecond; // catch sppe from wheelSpeed(with ctrl+left mice)
+        double right = wheelSpeeds.rightMetersPerSecond;
+
+        double leftVolt = leftPID.calculate(leftencoder.getRate(), left) + feedforward.calculate(left);
+        double rightVolt = rightPID.calculate(rightencoder.getRate(), right) + feedforward.calculate(right);
+
+        leftmotor.setVoltage(leftVolt);
+        rightmotor.setVoltage(rightVolt);
+        drive.feed();
+
+        SmartDashboard.putNumber("leftVolt", leftVolt);
+        SmartDashboard.putNumber("rightVolt", rightVolt);
+
+        SmartDashboard.putNumber("left", left);
+        SmartDashboard.putNumber("right", right);
+        SmartDashboard.putNumber("left_error", leftPID.getPositionError());
+        SmartDashboard.putNumber("right_error", rightPID.getPositionError());
+        SmartDashboard.putNumber("velocity", goal.velocityMetersPerSecond);
+
+        SmartDashboard.putNumber("chaspeedX", chaspeed.vxMetersPerSecond);
+        SmartDashboard.putNumber("chaspeedY", chaspeed.vyMetersPerSecond);
+
+        SmartDashboard.putNumber("errPosX", currentPose.minus(goal.poseMeters).getX());
+        SmartDashboard.putNumber("errPosY", currentPose.minus(goal.poseMeters).getY());
+
+        // m_driveSim.setInputs(leftVolt, rightVolt);//for simulation
+    }
+    public static void updateODO() {
+        var gyroAngle = Rotation2d.fromDegrees(-gyro.getAngle());
+        odometry.update(gyroAngle, leftencoder.getDistance(), rightencoder.getDistance());
+        field.setRobotPose(odometry.getPoseMeters());
+
+        SmartDashboard.putNumber("x", odometry.getPoseMeters().getX());
+        SmartDashboard.putNumber("y", odometry.getPoseMeters().getY());
+        SmartDashboard.putNumber("heading", odometry.getPoseMeters().getRotation().getDegrees());
+
+        kP = SmartDashboard.getNumber("kP", kP);
+        kI = SmartDashboard.getNumber("kI", kI);
+        kD = SmartDashboard.getNumber("kD", kD);
+
+        leftPID.setPID(kP, kI, kD);
+        rightPID.setPID(kP, kI, kD);
+        drive.feed();
+    }
+
+    public static void setODOPose(Pose2d pose) {
+        odometry.resetPosition(pose, pose.getRotation());
+        field.setRobotPose(odometry.getPoseMeters());
+    }
+    public static void resetOdo(){
+        odometry.setODOPose([0,0],0); // intended to set the position to the begining
+    }
+    public static void resetEnc(){
+        rightEncoder.reset()
+        leftEncoder.reset()
+    }
+    public static void resetGyro(){
+        gyro.reset()
+    }
+}
 
 
 
