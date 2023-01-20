81bf27041425c295e01b5a065ad16d18233ec798
ServandoDiaz <servandodiaz29@icloud.com>
2023-01-20 16:59:50 -0600

    Added centers and centroids to contours

--- src/main/java/frc/robot/rpiVision/vision.py
+++ src/main/java/frc/robot/rpiVision/vision.py
@@ -1,3 +1,6 @@
+#dont be racist 
+
+
 from tkinter import E
 import cv2
 import numpy as np
@@ -20,9 +23,6 @@ while(True):
     yellowupper_bound = np.array([30, 255, 255])
     purplelower_bound = np.array([129, 50, 70])
     purpleupper_bound = np.array([158, 255, 255])
-
-    yellow = yellowupper_bound, yellowlower_bound
-    purple = purplelower_bound, purpleupper_bound
 
     binary_imgyellow = cv2.inRange(hsv_img, yellowlower_bound, yellowupper_bound)
     cv2.imshow('yellow', binary_imgyellow)
@@ -50,19 +50,12 @@ while(True):
         x_list = [] #first is yellow second is purple
         y_list = []
         height, width, channel = frame.shape
-        #The largest rectangle we want to find
+        #The largest rectangle we find
         maxArea = 0
         maxContour = contours[0]
 
         #Find yellow
         for contour in contours:
-            # yellow_area = []
-            # yellow_area.append(cv2.contourArea(contour))
-            # if yellow_area.__len__ > 1:
-            # if yellow_area[0] > yellow_area[1]:
-            #     yellow_area.remove[1]
-            # else:
-            #     yellow_area.remove[0]
 
             #Ignore small contours that could be because of noise-bad thresholding
             Area = cv2.contourArea(contour)
@@ -77,33 +70,72 @@ while(True):
 
         rect = cv2.minAreaRect(maxContour) #record found time to draw
         center, size, angle = rect
+        # centerImage = cv2.circle(binary_imgyellow, (x,y) radius = 0, color = (0,0,0), thickness = 2)
         center = tuple([int(dim) for dim in center]) # Convert to int so we can draw
+        cv2.circle(img = output_img, center = center, radius = 2, color = (255,0,0), thickness = 10)
+      
+        # calculate moments for each contour
+        M = cv2.moments(maxContour)
+
+        # calculate x,y coordinate of center
+        if M["m00"] != 0:
+         cX = int(M["m10"] / M["m00"])
+         cY = int(M["m01"] / M["m00"])
+         cv2.circle(img = output_img, center = (cX,cY), radius = 2, color = (0,0,0), thickness = 10)
+      
+        else:
+            cX, cY = 0, 0
+        
         # Draw rectangle and circle
-        cv2.drawContours(output_img, [cv2.boxPoints(rect).astype(int)], -1, color = (0, 0, 255), thickness = 1)
+        cv2.drawContours(output_img, [cv2.boxPoints(rect).astype(int)], -1, color = (0, 0, 0), thickness = 5
+        )
 
         x_list.append((center[0] - width / 2) / (width / 2))
         y_list.append((center[1] - width / 2) / (width / 2))
 
     contours, hierarchy = cv2.findContours(binary_imgpurple, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    x_list = []
-    y_list = []
-    height, width, channel = frame.shape
+    if len(contours) != 0: #as long as there is a contour
+        x_list = [] #first is yellow second is purple
+        y_list = []
+        height, width, channel = frame.shape
+        #The largest rectangle we find
+        maxArea = 0
+        maxContour = contours[0]
     
-    #Find purple
-    for contour in contours:
+        #Find purple
+        for contour in contours:
 
-        #Ignore small contours that could be because of noise-bad thresholding
-        if cv2.contourArea(contour) < 1000:
-            continue
-        print(cv2.contourArea(contour))
-        #cv2.drawContours(output_img, contour, -1, color = (255, 255, 255), thickness = -1)
 
+
+            #Ignore small contours that could be because of noise-bad thresholding
+            if cv2.contourArea(contour) < 1000:
+                continue
+            print(cv2.contourArea(contour))
+            #cv2.drawContours(output_img, contour, -1, color = (255, 255, 255), thickness = -1)
+
+            if maxArea < Area: #new record! Update area and contour
+                    maxArea = Area
+                    maxContour = contour
+        
         rect = cv2.minAreaRect(maxContour)
         center, size, angle = rect
         center = tuple([int(dim) for dim in center]) # Convert to int so we can draw
+        cv2.circle(img = output_img, center = center, radius = 2, color = (255,0,0), thickness = 10)
+
+        # calculate moments for each contour
+        M = cv2.moments(maxContour)
+
+        # calculate x,y coordinate of center
+        if M["m00"] != 0:
+         cX = int(M["m10"] / M["m00"])
+         cY = int(M["m01"] / M["m00"])
+         cv2.circle(img = output_img, center = (cX,cY), radius = 2, color = (0,0,0), thickness = 10)
+      
+        else:
+            cX, cY = 0, 0
 
         # Draw rectangle and circle
-        cv2.drawContours(output_img, [cv2.boxPoints(rect).astype(int)], -1, color = (0, 0, 255), thickness = 1)
+        cv2.drawContours(output_img, [cv2.boxPoints(rect).astype(int)], -1, color = (0, 0, 0), thickness = 5)
 
         x_list.append((center[0] - width / 2) / (width / 2))
         y_list.append((center[1] - width / 2) / (width / 2))
@@ -124,9 +156,9 @@ while(True):
     # Convert the image to gray-scale
     # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Find the edges in the image using canny detector
-    img_blur = cv2.medianBlur(binary_imgyellow, 500)
-    img_blur = cv2.medianBlur(binary_imgpurple, 3)
-    edges = cv2.Canny("gray", 50, 200, apertureSize = 3)
+    # img_blur = cv2.medianBlur(binary_imgyellow, 500)
+    # img_blur = cv2.medianBlur(binary_imgpurple, 3)
+    # edges = cv2.Canny("gray", 50, 200, apertureSize = 3)
     # Detect points that form a line
     # lines = cv2.HoughLinesP(edges, 1, np.pi/180, max_slider=3, minLineLength=10, maxLineGap=250)
 
@@ -148,3 +180,6 @@ while(True):
     # cv2.imshow("Result Image", binary_imgpurple)
 
     # input("Press any key to continue...")
+    # def s():
+    #  1/0
+    # s()    
